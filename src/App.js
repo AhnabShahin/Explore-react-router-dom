205068c08cf6832834daf09fbaa06020b2e33f38
shahin <shahinahnab@gmail.com>
2021-10-05 23:37:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Cart from './components/Cart/Cart';
+import AboutUS from './components/AboutUS/AboutUS';
+import Contact from './components/Contact/Contact';
+import Services from './components/Services/Services';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pages, SetPages] = useState({});
+
+  useEffect(() => {
+    fetch('./Driver.JSON')
+      .then(res => res.json())
+      .then(data => SetPages(data))
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Router>
+        <Header></Header>
+        <Switch>
+
+          <Route exact path="/">
+            <Home home={pages.home}></Home>
+            <Cart home={pages.home}></Cart>
+          </Route>
+          <Route exact path="/home">
+            <Home home={pages.home}></Home>
+            <Cart home={pages.home}></Cart>
+          </Route>
+          <Route exact path="/aboutUs">
+            <AboutUS aboutUs={pages.aboutUs}></AboutUS>
+          </Route>
+          <Route exact path="/contact">
+            <Contact contactUs={pages.contactUs}></Contact>
+          </Route>
+          <Route exact path="/services">
+            <Services ourServices={pages.ourServices}></Services>
+          </Route>
+
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
